09a2c29300a0fa4c0afc77aa039e9bfcaba56ce2
Alex <arekusunaito@gmail.com>
2022-03-21 13:06:44 -0600

    Install package

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -1,8 +1,8 @@
+rm -rf ./node_modules
 touch .npmrc
 echo "//npm.pkg.github.com/:_authToken=$NPM_TOKEN
 @little-universe:registry=https://npm.pkg.github.com" > .npmrc
 npm install @little-universe/do-not-allow-missing-properties --registry=https://npm.pkg.github.com/
 rm .npmrc
-cat .npmrc
 npm ci
 echo "End execution"
