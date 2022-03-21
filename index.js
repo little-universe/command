b05b32c467fcf1ccafdba2ca1c4283a6b01b6f14
Alex <arekusunaito@gmail.com>
2022-03-21 13:08:31 -0600

    Check for node install

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// module.exports = require('./src/command.js')
-const foo = require('@little-universe/do-not-allow-missing-properties');
-console.log(foo)
-if("doNotAllowMissingProperties" in foo)
-{
-    console.log("Wow, nice!");
-}
+// // module.exports = require('./src/command.js')
+// const foo = require('@little-universe/do-not-allow-missing-properties');
+// console.log(foo)
+// if("doNotAllowMissingProperties" in foo)
+// {
+//     console.log("Wow, nice!");
+// }
+
+console.log("OK!")
