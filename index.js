43ac700a03cb88025077fb7738ec14d339167976
Alex <arekusunaito@gmail.com>
2022-03-21 13:13:34 -0600

    Checking if node got it

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // // module.exports = require('./src/command.js')
-// const foo = require('@little-universe/do-not-allow-missing-properties');
-// console.log(foo)
-// if("doNotAllowMissingProperties" in foo)
-// {
-//     console.log("Wow, nice!");
-// }
+const foo = require('@little-universe/do-not-allow-missing-properties');
+if("doNotAllowMissingProperties" in foo)
+{
+    console.log("Wow, nice!");
+}
 
 console.log("OK!")
